529cfe57b2ecdddf61797e3124b55698921af52b
jaimetrovoada <jaimetrovoada@gmail.com>
2023-06-30 16:12:37 +0800

    update schema error message

--- lib/validators/schemas.ts
+++ lib/validators/schemas.ts
@@ -1,8 +1,8 @@
 import * as z from "zod";
 
 export const ApplicationValidator = z.object({
-  title: z.string().min(1),
-  company: z.string().min(1),
+  title: z.string().min(1, "Title must contain at least 1 character(s)"),
+  company: z.string().min(1, "Company must contain at least 1 character(s)"),
   status: z.enum(["APPLIED", "INTERVIEW", "OFFER", "REJECTED", "SAVED"]),
   postingUrl: z.string().url(),
   dateApplied: z.coerce.date(),
