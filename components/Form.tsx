2572d553aed28fd3621fb5c7193e2776bd267a3a
jaimetrovoada <jaimetrovoada@gmail.com>
2023-06-30 16:13:00 +0800

    create form and input components

---
+++ components/Form.tsx
@@ -0,0 +1,59 @@
+import { getClasses } from "@/lib/utils";
+import {
+  Path,
+  UseFormRegister,
+  RegisterOptions,
+  FieldError,
+} from "react-hook-form";
+type FormProps = React.HTMLAttributes<HTMLFormElement>;
+
+type InputProps<TFormValues> = {
+  label: string;
+  register: UseFormRegister<TFormValues>;
+  rules?: RegisterOptions;
+  name: Path<TFormValues>;
+  error?: FieldError;
+} & Omit<React.InputHTMLAttributes<HTMLInputElement>, "name">;
+
+const Form = ({ ...props }: FormProps) => {
+  return (
+    <form
+      {...props}
+      className={getClasses(
+        "mx-auto flex w-full max-w-screen-sm flex-col gap-6 rounded-lg border border-gray-600/50 bg-zinc-950 p-4 shadow-md",
+        props.className
+      )}
+    >
+      {props.children}
+    </form>
+  );
+};
+
+const Input = <TFormValues extends Record<string, unknown>>({
+  className,
+  name,
+  label,
+  register,
+  rules,
+  error,
+  ...props
+}: InputProps<TFormValues>) => {
+  return (
+    <div className={getClasses("flex flex-col", className)}>
+      <label htmlFor={name}>{label}</label>
+      <input
+        {...props}
+        {...register(name, rules)}
+        id={name}
+        className={getClasses(
+          "rounded border border-gray-400/40 bg-zinc-900 p-2 placeholder:capitalize",
+          className
+        )}
+      />
+      {error && <p className="text-xs text-red-600">{error.message}</p>}
+    </div>
+  );
+};
+
+Form.Input = Input;
+export default Form;
--- components/index.ts
+++ components/index.ts
@@ -1,2 +1,4 @@
 export { default as Header } from "./Header";
 export { default as Button } from "./Button";
+export { default as Form } from "./Form";
+export { default as JobForm } from "./Job.form";
