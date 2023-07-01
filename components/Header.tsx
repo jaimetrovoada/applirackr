b0e46a1940aa46c6cc70c955b7657332b7ac9c83
jaimetrovoada <jaimetrovoada@gmail.com>
2023-07-01 11:56:48 +0800

    add sign out button to header

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,8 @@
 import { User } from "next-auth";
 import Image from "next/image";
 import Button from "./Button";
-import { signIn } from "next-auth/react";
+import { signIn, signOut } from "next-auth/react";
+import { useRef } from "react";
 
 interface Props {
   user: Omit<User, "id"> | undefined;
@@ -14,13 +15,18 @@ const Header = ({ user }: Props) => {
       <nav className="container mx-auto flex flex-row items-center justify-between p-4 lg:px-0">
         <h1 className="text-3xl lg:text-6xl">AppliTrackr</h1>
         {user ? (
-          <div className="relative w-12 aspect-square rounded-full overflow-hidden">
-            <Image
-              src={user.image!}
-              alt="user avatar"
-              fill
-              className="object-cover"
-            />
+          <div className="flex flex-row items-center">
+            <div className="relative aspect-square w-12 rounded-full border border-gray-600/50 bg-black lg:w-20">
+              <Image
+                src={user.image!}
+                alt="user avatar"
+                fill
+                className="object-cover"
+              />
+            </div>
+            <Button className="-ml-6 pl-8" onClick={() => signOut()}>
+              sign out
+            </Button>
           </div>
         ) : (
           <Button onClick={() => signIn()}>Sign in</Button>
