2eaea5120fccbbcd5d35b8020ab4f7f19d752957
jaimetrovoada <jaimetrovoada@gmail.com>
2023-07-11 13:53:03 +0800

    fix height

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -9,7 +9,7 @@ export default function Page() {
   const searchParams = useSearchParams();
   return (
     <main className="container mx-auto flex flex-1 flex-col items-center justify-center gap-4 p-4 lg:px-0">
-      <div className="mx-auto flex h-1/3 w-full max-w-screen-sm flex-col items-center gap-6 rounded-lg border border-gray-600/50 bg-zinc-950 p-4 shadow-md">
+      <div className="min-h-1/3 mx-auto flex w-full max-w-screen-sm flex-col items-center gap-6 rounded-lg border border-gray-600/50 bg-zinc-950 p-4 shadow-md">
         <h2 className="text-4xl font-semibold">Sign to AppliTrackr</h2>
         <div className="my-auto flex flex-col gap-4">
           <Button
